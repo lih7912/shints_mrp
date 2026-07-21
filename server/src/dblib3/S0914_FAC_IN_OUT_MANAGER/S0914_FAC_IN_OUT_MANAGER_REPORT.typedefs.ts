0a2c4eb0ee87972e85e7db4f8ff3c889031a4f9d
lih7912 <lih7912@autostock.co.kr>
2026-07-21 16:40:02 +0900

    #96 수정중

--- server/src/dblib3/S0914_FAC_IN_OUT_MANAGER/S0914_FAC_IN_OUT_MANAGER_REPORT.typedefs.ts
+++ server/src/dblib3/S0914_FAC_IN_OUT_MANAGER/S0914_FAC_IN_OUT_MANAGER_REPORT.typedefs.ts
@@ -3,6 +3,13 @@ import { gql } from 'apollo-server-express';
 const typeDefs = gql`
     input I_S0914_FAC_IN_OUT_MANAGER_REPORT {
         PO_CD: String
+        ORDER_CD: String
+        SUPPLIER: String
+        DESCRIPTION: String
+        UNIT: String
+        MATL_CD: String
+        COLOR: String
+        SPEC: String
     }
 
     type O_S0914_FAC_IN_OUT_MANAGER_REPORT {
@@ -13,6 +20,9 @@ const typeDefs = gql`
     # Query 정의
     type Query {
         mgrQuery_S0914_FAC_IN_OUT_MANAGER_REPORT(
+            data: I_S0914_FAC_IN_OUT_MANAGER_REPORT!
+        ): [O_S0914_FAC_IN_OUT_MANAGER_REPORT!]!
+        mgrQuery_S0914_FAC_IN_OUT_MANAGER_REPORT2(
             data: I_S0914_FAC_IN_OUT_MANAGER_REPORT!
         ): [O_S0914_FAC_IN_OUT_MANAGER_REPORT!]!
     }
